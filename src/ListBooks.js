ca0800531f1e3b7a1eab3749fb9bbf70b065a51b
Gil Akos <gil@astra.finance>
2017-12-02 07:55:29 -0800

    Feat: implement book changer

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -8,56 +8,64 @@ class ListBooks extends Component {
         id: 0,
         title: 'To Kill a Mockingbird',
         author: 'Harper Lee',
-        bookshelf: 'Currently Reading',
+        bookshelf: 'currentlyReading',
         cover: 'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api',
       },
       {
         id: 1,
         title: "Ender's Game",
         author: 'Orson Scott Card',
-        bookshelf: 'Currently Reading',
+        bookshelf: 'currentlyReading',
         cover: 'http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api',
       },
       {
         id: 2,
         title: "1776",
         author: 'David McCullough',
-        bookshelf: 'Want to Read',
+        bookshelf: 'wantToRead',
         cover: 'http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api',
       },
       {
         id: 3,
         title: "Harry Potter and the Sorcerer's Stone",
         author: 'J.K. Rowling',
-        bookshelf: 'Want to Read',
+        bookshelf: 'wantToRead',
         cover: 'http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api',
       },
       {
         id: 4,
         title: "The Hobbit",
         author: 'The Hobbit',
-        bookshelf: 'Read',
+        bookshelf: 'read',
         cover: 'http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api',
       },
       {
         id: 5,
         title: "Oh, the Places You'll Go!",
         author: 'Seuss',
-        bookshelf: 'Read',
+        bookshelf: 'read',
         cover: 'http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api',
       },
       {
         id: 6,
         title: "The Adventures of Tom Sawyer",
         author: 'Mark Twain',
-        bookshelf: 'Read',
+        bookshelf: 'read',
         cover: 'http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api',
       },
     ]
   }
 
+  handleShelfUpdate = (shelf, book) => {
+    const newbooks = this.state.books
+    newbooks[book.id].bookshelf = shelf
+    this.setState((state) => {
+      books: newbooks
+    })
+  }
+
   render(){
-    console.log(this.state.books)
+    //console.log(this.state.books)
     return(
       <div className="list-books">
         <div className="list-books-title">
@@ -69,13 +77,13 @@ class ListBooks extends Component {
               <h2 className="bookshelf-title">Currently Reading</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  {this.state.books.filter(book => book.bookshelf==='Currently Reading').map((book) =>
+                  {this.state.books.filter(book => book.bookshelf==='currentlyReading').map((book) =>
                     <li>
                       <div className="book">
                         <div className="book-top">
                           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.cover})` }}></div>
                           <div className="book-shelf-changer">
-                            <select>
+                            <select onChange={(event) => this.handleShelfUpdate(event.target.value, book)}>
                               <option value="none" disabled>Move to...</option>
                               <option value="currentlyReading">Currently Reading</option>
                               <option value="wantToRead">Want to Read</option>
@@ -97,13 +105,13 @@ class ListBooks extends Component {
               <h2 className="bookshelf-title">Want to Read</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  {this.state.books.filter(book => book.bookshelf==='Want to Read').map((book) =>
+                  {this.state.books.filter(book => book.bookshelf==='wantToRead').map((book) =>
                     <li>
                       <div className="book">
                         <div className="book-top">
                           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.cover})` }}></div>
                           <div className="book-shelf-changer">
-                            <select>
+                            <select onChange={(event) => this.handleShelfUpdate(event.target.value, book)}>
                               <option value="none" disabled>Move to...</option>
                               <option value="currentlyReading">Currently Reading</option>
                               <option value="wantToRead">Want to Read</option>
@@ -125,13 +133,13 @@ class ListBooks extends Component {
               <h2 className="bookshelf-title">Read</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  {this.state.books.filter(book => book.bookshelf==='Read').map((book) =>
+                  {this.state.books.filter(book => book.bookshelf==='read').map((book) =>
                     <li>
                       <div className="book">
                         <div className="book-top">
                           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.cover})` }}></div>
                           <div className="book-shelf-changer">
-                            <select>
+                            <select onChange={(event) => this.handleShelfUpdate(event.target.value, book)}>
                               <option value="none" disabled>Move to...</option>
                               <option value="currentlyReading">Currently Reading</option>
                               <option value="wantToRead">Want to Read</option>
